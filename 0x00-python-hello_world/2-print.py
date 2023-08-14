5a0b000dcb7a291aef55e6c0127013173d853564
Willard-Masara <willardmasarat@gmail.com>
2023-08-14 03:21:46 -0700

    this is python

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# this is to print the line exactly, no mistake
+
+
+print("\"Programming is like building a multilingual puzzle\n")
---
+++ 0x00-python-hello_world/print_my_name.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# this is to print only my name
+
+
+print("my name is willard\n")
