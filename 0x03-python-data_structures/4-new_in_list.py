87260177be8ea7db605da95978d5dbc69a4a51b2
Willard-Masara <willardmasarat@gmail.com>
2023-08-14 18:59:18 -0700

    new cha nges

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+# task 3
+
+
 def new_in_list(my_list, idx, element):
     my_list.insert(idx, element)
     for val in my_list:
         if val < 0 or val >= len(my_list):
             return my_list[:]
     return my_list[:]
-
-
-original_list = [10, 20, 30, 40, 50]
-new_list = new_in_list(original_list, 2, 35)
-print(original_list)
-print(new_list) 
-
