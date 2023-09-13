cf50e53702eb29ed7677a98789751635890d5f7c
Willard-Masara <willardmasarat@gmail.com>
2023-09-13 06:55:27 -0700

    testing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# this is task 0
+"""this is task 0-- opening a file"""
 
 
 def read_file(filename=""):
-	"""oepning a file in pythin"""
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            for line in file:
-                print(line, end="")
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
+    """use resad to print file output"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
