982b604b908e2f2b7b8d61653fcf8bd562a48473
Willard-Masara <willardmasarat@gmail.com>
2023-10-01 23:36:41 -0700

    pycodestyle issues

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" Defines an object atribute function """
+"""
+Defines an object atribute function
+"""
+
 
 def lookup(obj):
-	""" this function returns the attributes of the object"""
+    """ this function returns the attributes of the object"""
     check = dir(obj)
     return check
