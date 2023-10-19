af0ae69b5eaa3e1084afbc54c5d70d572d8cf29b
Willard-Masara <willardmasarat@gmail.com>
2023-10-18 19:05:29 -0700

    advanced tasks fixing last checker

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,15 +1,13 @@
 -- Selecting tv_shows.title from shows without the genre "Comedy"
 -- by joining tv_shows, tv_show_genres, and tv_genres tables
-
--- Step 1: Select tv_shows.title from tv_shows table
-SELECT tv_shows.title
--- Step 2: Left join tv_show_genres table to tv_shows table using show_id
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
--- Step 3: Left join tv_genres table to tv_show_genres table using genre_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
--- Step 4: Filter shows where genre is not "Comedy" (genre_id for Comedy is 5)
-WHERE tv_genres.id IS NULL OR tv_genres.id != 5
--- Step 5: Order results in ascending order by show title
-ORDER BY tv_shows.title ASC;
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;
 
--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,12 +1,7 @@
 -- Selecting tv_shows.title and the sum of ratings for each show
 -- by joining tv_shows with tv_show_ratings table and calculating rating sums
-
--- Step 1: Selecting show titles from tv_shows table and sum of ratings from tv_show_ratings table
-SELECT CONCAT(tv_shows.title, ' - ', IFNULL(SUM(tv_show_ratings.rate), 0)) AS 'Show - Rating Sum'
--- Step 2: Left join tv_show_ratings table to tv_shows table using show_id
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
--- Step 3: Group the results by show titles
-GROUP BY tv_shows.title
--- Step 4: Order results in descending order by rating sum
-ORDER BY SUM(tv_show_ratings.rate) DESC;
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
