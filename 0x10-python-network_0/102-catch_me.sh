a2544849ba6d7855060461dbf9dd83f9c1da2841
Willard-Masara <willardmasarat@gmail.com>
2023-11-30 14:19:21 -0800

    on the advanced three

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Make a request to cause the server to respond with "You got me!"
-curl -s -X PUT -L --data "user_id=98" 0.0.0.0:5000/catch_me
+#YOu got me thre
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
 
